84ded6c2b040d825eace8a89c3da5c1fdd8733cd
ToolLab <toollab@toollab.kr>
2025-02-17 02:04:59 +0900

    배포 오류 수정

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -1,24 +1,55 @@
 class SitemapsController < ApplicationController
-  # 인증이나 CSRF 체크 비활성화
+  # 정적 페이지 상수 정의
+  STATIC_PAGES = %w[privacy terms about].freeze
+  CACHE_DURATION = 14400 # 4시간
+  BATCH_SIZE = 500 # 배치 크기
+
   skip_before_action :verify_authenticity_token
-  skip_before_action :authenticate_user!
-
+  before_action :set_headers
+  
   def show
-    # ETag과 Last-Modified를 이용한 HTTP 캐싱
-    @phone_numbers = PhoneNumber.select(:number, :updated_at).order(updated_at: :desc)
-    @last_modified = [@phone_numbers.first&.updated_at, Time.current.beginning_of_day].compact.max
-
+    set_variables
+    set_caching_headers
+    
+    # HTTP 캐싱 적용
     fresh_when(
-      etag: [@phone_numbers.cache_key_with_version, @last_modified],
+      etag: cache_key,
       last_modified: @last_modified,
       public: true
     )
+  end
 
-    # 캐시 헤더 설정 (이미 fresh_when에서 설정되었지만, 명시적으로 추가)
-    headers["Content-Type"] = "application/xml"
-    headers["Cache-Control"] = "public, max-age=14400" # 4시간 캐시
+  private
 
+  def set_variables
     @host = request.protocol + request.host_with_port
+    @phone_numbers = PhoneNumber
+      .select(:number, :updated_at)
+      .order(updated_at: :desc)
+      .includes(:comments) # N+1 쿼리 방지
+    
+    @last_modified = [
+      @phone_numbers.first&.updated_at,
+      Time.current.beginning_of_day
+    ].compact.max
+  end
+
+  def set_headers
+    headers.merge!({
+      "Content-Type" => "application/xml",
+      "Cache-Control" => "public, max-age=#{CACHE_DURATION}",
+      "X-Robots-Tag" => "noindex"
+    })
+  end
+
+  def cache_key
+    [
+      "sitemap",
+      @phone_numbers.cache_key_with_version,
+      @last_modified.to_i,
+      STATIC_PAGES.hash
+    ]
+  end
     
     respond_to do |format|
       format.xml
