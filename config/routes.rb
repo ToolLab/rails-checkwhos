605670b989590dc995748a9fe091e3e07826dd96
ToolLab <toollab@toollab.kr>
2025-02-17 15:23:51 +0900

    devise 라우트 수저

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   # Sitemap
   get "sitemap.xml", to: "sitemaps#show", defaults: { format: "xml" }
 
+  # Devise 라우팅
+  devise_for :users
+
   # 루트 경로를 /kr로 리다이렉트
   root to: redirect("/kr")
 
   # 한국어 사이트 라우팅 (/kr)
   scope "/:locale", locale: /kr/ do
-    # devise 라우트를 scope 안으로 이동
-    devise_for :users, path: ""
-
     get "/", to: "home#index", as: :locale_root
     get "search/:number", to: "home#search", as: :search
 
@@ -25,10 +25,8 @@ Rails.application.routes.draw do
     end
   end
 
-  # scope 밖의 모든 경로를 /kr로 리다이렉트
-  get "/*path", to: redirect("/kr/%{path}"), constraints: lambda { |req|
-    !req.path.start_with?("/kr/") && !req.path.start_with?("/up")
-  }
+  # 검색 관련 경로만 /kr로 리다이렉트
+  get "/search/*path", to: redirect("/kr/search/%{path}")
 
   # 상태 체크 엔드포인트
   get "up" => "rails/health#show", as: :rails_health_check
